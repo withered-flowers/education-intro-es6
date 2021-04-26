f01cd77990f29471a17cc5489adc6ea090422d9b
Withered Flowers <withered-flowers@users.noreply.github.com>
2021-04-27 06:51:35 +0700

    fix: typo

--- sources/new/source-08-destructuring-simplified-import.js
+++ sources/new/source-08-destructuring-simplified-import.js
@@ -1,10 +1,10 @@
 const { 
-  fungsiPenjumlahan,
+  fungsiPenjumlahan1,
   fungsiPenjumlahan2,
   fungsiPenjumlahan3,
   fungsiPenjumlahan4
 } = require('./source-06a-module-exports.js')
 
 // driver code start here
-fungsiPenjumlahan(10, 20);
+fungsiPenjumlahan1(10, 20);
 fungsiPenjumlahan2(20, 30);
